fd4bd3f764c2c9cb50f4398a8d4b362f6b6fa95a
Scully87 <scully87.ts@googlemail.com>
2014-10-18 23:57:11 +0100

    can record a simple strike without bonuses

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -19,13 +19,22 @@ describe('UpYourAlley', function() {
    
     it('record a gutter game', function() {
       rollCount.call(game, 0, 20);
-      //The call() method calls a function with a this value and arguments.
+      //The call() method calls a function with,
+      // a '.this' value and arguments (pins, number of rolls).
       expect(game.score()).toEqual(0);
     });
     
     it('record a game of one pin each roll', function() {
       rollCount.call(game, 1, 20);
-      expect(game.score()).toEqual(20)
+      expect(game.score()).toEqual(20);
+    });
+
+    it('record a strike', function() {
+      game.roll(10)
+      // adds 10 to the score without using single pin count,
+      // 18 rolls remain after 1 strike
+      rollCount.call(game, 0, 18)
+      expect(game.score()).toEqual(10);
     });
 
   });
